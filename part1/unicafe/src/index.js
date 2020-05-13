c80154abd910bba434982cff8081139e267e387e
iulover99 <iulover99@gmail.com>
2020-05-13 14:37:49 +0700

    1.8 done

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,6 +1,19 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
+const Statictics = ({ good,neutral,bad }) => {
+  return (
+    <div>
+      <h1>statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {good + neutral + bad}</p>
+      <p>average {(good-bad)/(good + neutral + bad)}</p>
+      <p>positive {good/(good + neutral + bad)*100} %</p>
+    </div>
+  )
+}
 const App = () => {
   // save clicks of each button to own state
   const [good, setGood] = useState(0)
@@ -13,13 +26,16 @@ const App = () => {
       <button onClick={() => setGood(good + 1)}>good</button>
       <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
       <button onClick={() => setBad(bad + 1)}>bad</button>
+      <Statictics good={good} neutral={neutral} bad={bad} />
+      {/* 
       <h1>statistics</h1>
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
       <p>all {good + neutral + bad}</p>
       <p>average {(good-bad)/(good + neutral + bad)}</p>
-      <p>positive {good/(good + neutral + bad)*100} %</p>
+      <p>positive {good/(good + neutral + bad)*100} %</p> 
+      */}
     </div>
   )
 }
