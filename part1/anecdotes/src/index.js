e0036d936aaddbdf457c672116b460bcee953e34
iulover99 <iulover99@gmail.com>
2020-05-14 01:10:26 +0700

    part1 done completely

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -1,10 +1,13 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
+
 const App = (props) => {
   const [selected, setSelected] = useState(0)
 
   const [ points, setPoint ] = useState(new Array(anecdotes.length).fill(0))
+
+  const [ mostVote, setMostVote] = useState ({max:0, mostVotePosition:0})
 
   const handleRandomAnecdotes = () => {  
     setSelected(Math.floor(Math.random() * props.anecdotes.length))
@@ -15,15 +18,26 @@ const App = (props) => {
     const copy = [...points]
     copy.splice(selected,1,copy[selected]+1)
     setPoint(copy)
+    copy.forEach(element => {
+      if(element>mostVote.max) {
+        const newMax = { max : element, mostVotePosition : copy.indexOf(element)}
+        setMostVote(newMax)
+      }
+    })
     console.log(copy);
   }
 
+
   return (
     <div>
+      <h1>Anecdote of he day</h1>
       <p>{props.anecdotes[selected]}</p>
       <button onClick={handlePoint}>vote</button>
-
       <button onClick={handleRandomAnecdotes}>next anecdotes</button>
+
+      <h1>Anecdote with most votes</h1>
+      <p>{props.anecdotes[mostVote.mostVotePosition]}</p>
+      <p>has {mostVote.max} votes</p>
     </div>
   )
 }
