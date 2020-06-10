ccfeac5fa293e93add81d31f823853a86ff6c9c4
iulover99 <iulover99@gmail.com>
2020-06-10 16:47:22 +0700

    Part2.5 done

--- part2/src/index.js
+++ part2/src/index.js
@@ -1,54 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
-const Header = ({name}) => {
-  return (
-    <div>
-      <h2>
-        {name}
-      </h2>
-    </div>
-  )
-}
-
-const Part = ({name,exercises}) => {
-  return (
-    <div>
-      <p>{name} {exercises}</p>
-    </div>
-  )
-}
-
-const Content = ({parts}) => {
-  return (
-    <div>
-      {parts.map((part) =>
-        <Part key= {part.id} name={part.name} exercises={part.exercises} />
-      )}
-    </div>
-  )
-}
-
-const Total = ({parts}) => {
-  let exercises = parts.map((part)=> part.exercises);
-  console.log(exercises)
-  return (
-    <div>
-      <p>Total of {exercises.reduce((accumulator, currentValue) => accumulator+currentValue,0)} exercises</p>
-    </div>
-  )
-}
-
-const Course = ({course}) => {
-  return (
-    <div>
-      <Header name={course.name}/>
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </div>
-  )
-}
-
+import Course from './components/Course'
 
 
 const App = () => {
